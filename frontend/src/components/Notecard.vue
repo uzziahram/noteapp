ac9107ce12e7721e504236bffc0e2a48fbd4e64b
ugenella <uzziahgenella@gmail.com>
2025-09-17 15:53:54 +0800

    Frontend updates 3

---
+++ frontend/src/components/Notecard.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1 class="font-extrabold"> {{ note.title }}</h1>
+    <p> {{ note.content }}</p>
+
+</div>
+</template>
+
+<script setup>
+
+    const prop = defineProps({
+        note: {
+            type: Object,
+            required: true
+        }
+    })
+
+</script>
